e6c0866fb3d2b872df313fb2450a319aab5b8377
wilkie <wilkie05@gmail.com>
2012-02-13 00:03:10 -0500

    Adding a button for no good reason.

--- lib/statusaur.rb
+++ lib/statusaur.rb
@@ -9,6 +9,11 @@ module Statusaur
   def self.run
     Shoes.app :title => "Statusaur", :width => 300, :height => 100 do
       para "HEY"
+      button "Accounts" do
+        window :title => "Accounts" do
+          para "Foo"
+        end
+      end
     end
   end
 end
